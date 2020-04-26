4575960e8e03d07b3bac4732a1d56b225fed5924
Jeko <jeko@mac.com>
2020-04-26 13:50:30 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,55 @@
+# homebridge-http-switch-webhook
+
+This Project deliver a simple, but fail-safe HTTP Switch for HomeBridge with optionnal support for webhook with or without conjuction of polling.
+
+To simplify the webhook, it listens on a port and simply triggers the "status_url" parameter on request. So it needs no parameters.
+```shell
+http://homebridge_ip:webhook_port
+```
+
+## Installation
+
+```shell
+npm install -g homebridge-http-switch-webhook
+```
+
+## config.json
+
+The following paramteres are supported:
+
+```json
+    // Switch with polling and a On/Off-State
+    {
+      "accessory" : "SimpleHttpSwitch",
+      "http_method" : "GET",                              // The HTTP-Method
+      "ignore_https_security" : false                     // Should the HTTPS Certificate (for all https requests) be validated? (Set it to true if you are using a self-signed cert)
+      "set_off_url" : "http://localhost/turn/off",        // The JSON-Webservice URL for turning the device on
+      "status_url" : "http://localhost/device/status",    // The JSON-Webservice URL for getting the device's status
+      "on_if_this_fn" : "(obj)=>obj.status ? obj.status=='ON' : null", //JS Function for evaluating if the device is on, you can alternativly use the following:
+      "on_if_this": {"status":"on"},                      // If you don't want to use on_if_this_fn
+      "off_if_this": {"status":"off"},                    // If you don't want to use on_if_this_fn
+      "set_on_url" : "http://localhost/turn/on",          // The JSON-Webservice URL for turning the device on
+      "polling" : true,                                   // Enable Polling/Refreshing of the Status
+      "pollingInterval" : 5,                              // Polling Interval in Seconds
+      "name" : "Desk Light",                              // Name of your Switch/Accessory
+      "webhook_port" : "3211"                             // (Optionnal) Port to listen for webhook
+    }
+
+    // Stateless Switch, will automaticly go off a few miliseconds after switched on
+    {
+      "accessory" : "SimpleHttpSwitch",
+      "set_on_url" : "http://localhost/trigger/something",
+      "status_url" : "", // Needs to be empty
+      "name" : "Desk Light"
+    }
+```
+
+## ToDo
+
+*   migrate to a platform instead of a single accessory
+*   support other HomeKit-Accessories then 'Switch'
+*   migrate to another web server than the now deprecated "express" one.
+
+## Credits
+[https://github.com/sinnaj-r/homebridge-simple-http](https://github.com/sinnaj-r/homebridge-simple-http)
+
---
+++ index.js
@@ -0,0 +1,178 @@
+var request = require("request-promise-native");
+var express = require('express'),
+    bodyParser = require('body-parser'),
+    app = express();
+var Service, Characteristic;
+
+module.exports = function(homebridge) {
+  Service = homebridge.hap.Service
+  Characteristic = homebridge.hap.Characteristic
+  homebridge.registerAccessory("homebridge-http-switch-webhook", "httpSwitchWebhook", httpSwitchWebhookAccessory)
+}
+
+function httpSwitchWebhookAccessory(log, config) {
+  this.log = log;
+
+  // URLs
+  this.status_url = config["status_url"] || false
+  this.set_on_url = config["set_on_url"]
+  this.set_off_url = config["set_off_url"]
+
+  // HTTP Stuff
+  this.http_method = config["http_method"] || "GET"
+  this.ignore_https_security = config["ignore_https_security"] || false
+
+  // State Stuff
+  this.on_ifth_is = config["on_ifth_is"]
+  this.off_if_this = config["off_if_this"]
+
+  // Polling Stuff
+  this.polling = config["polling"] || false
+  this.pollingInterval = parseInt(config["pollingInterval"] || 5, 10) // In Seconds
+
+  this.on_if_this_fn =
+      config["on_if_this_fn"] && eval(config["on_if_this_fn"])
+  // General
+  this.name = config["name"]
+
+  // Webhook Stuff
+  this.webhook_port = config["webhook_port"] || "3211"
+
+  if (this.webhook_port != "") {
+    if (this.status_url == "") {
+      this.log("Status url missing for webhook. Web server will not start.")
+    } else {
+      app.use(bodyParser.json())
+      app.get('/', (req, res) => {
+          var ret = req.query
+          var json = JSON.stringify(ret)
+          this.log('Webhook received')
+          res.send('Got it!')
+          this.switchService.getCharacteristic(Characteristic.On).getValue()
+      })
+      app.listen(this.webhook_port, () => {
+          this.log('Listening on port %s', this.webhook_port)
+      })
+    }
+  }
+}
+
+httpSwitchWebhookAccessory.prototype.getServices = function () {
+    var package = require('./package.json')
+    var informationService = new Service.AccessoryInformation()
+    informationService
+        .setCharacteristic(Characteristic.Manufacturer, "Homebridge")
+        .setCharacteristic(Characteristic.Model, "HTTP Switch webhook")
+        .setCharacteristic(Characteristic.SerialNumber, package.version)
+    this.switchService = new Service.Switch()
+    if (this.status_url) {
+        this.switchService
+            .getCharacteristic(Characteristic.On)
+            .on("get", this.getState.bind(this))
+            .on("set", this.setState.bind(this))
+    }
+    else {
+        this.switchService
+            .getCharacteristic(Characteristic.On)
+            .on("get", this.getState.bind(this))
+            .on("set", this.setState.bind(this))
+    }
+    if (this.polling) {
+        this.statePolling()
+    }    
+    return [this.switchService, informationService]
+}
+
+httpSwitchWebhookAccessory.prototype.makeRequest = function (url) {
+  if (this.ignore_https_security == true) {
+    const agentOptions = {
+        rejectUnauthorized: false
+    }
+    return request({
+        url,
+        agentOptions,
+        method: this.http_method,
+        json: true
+    })
+  } else {
+      return request(url, {
+          method: this.http_method,
+          json: true
+      })
+  }
+}
+
+httpSwitchWebhookAccessory.prototype.getState = function (callback) {
+  var _this = this;
+  if (!this.status_url) {
+      callback(null, false);
+      return;
+  }
+  this.makeRequest(this.status_url)
+      .then(function (res) {
+      var ret = res;
+      var retString = JSON.stringify(ret);
+      if (_this.on_if_this_fn) {
+          var onStatus = _this.on_if_this_fn(ret);
+          if (onStatus !== null) {
+              callback(null, onStatus);
+              _this.log("HTTP state get function succeeded! (" + retString + ")");
+              return;
+          }
+          callback(Error("Status function not known"));
+          _this.log("Status function not known");
+          return;
+      }
+      if (retString == JSON.stringify(_this.on_if_this)) {
+          callback(null, true);
+          _this.log("HTTP state get succeeded! (" + retString + ")");
+      }
+      else if (retString == JSON.stringify(_this.off_if_this)) {
+          callback(null, false);
+          _this.log("HTTP state get succeeded! (" + retString + ")");
+      }
+      else {
+          callback(Error("Status not known"));
+          _this.log("Status not known");
+      }
+  })
+      .catch(function (err) {
+      _this.log("HTTP state get failed! (" + err + ")");
+      callback(err);
+  });
+}
+
+httpSwitchWebhookAccessory.prototype.statePolling = function () {
+  clearTimeout(this.pollingTimeOut);
+  this.log("[" + this.name + "] POLLING STATUS");
+  this.switchService.getCharacteristic(Characteristic.On).getValue();
+  this.pollingTimeOut = setTimeout(this.statePolling.bind(this), this.pollingInterval * 1000);
+}
+
+httpSwitchWebhookAccessory.prototype.setState = function (powerOn, callback) {
+  var _this = this;
+  var body;
+  var uri = powerOn ? this.set_on_url : this.set_off_url;
+  uri = this.status_url ? uri : this.set_on_url;
+  this.makeRequest(uri)
+      .then(function (res) {
+      _this.log("HTTP power function succeeded! (" + JSON.stringify(res) + ")");
+      if (!_this.status_url) {
+          setTimeout(function () {
+              return _this.switchService
+                  .getCharacteristic(Characteristic.On)
+                  .getValue();
+          }, 300);
+      }
+      callback();
+  })
+      .catch(function (err) {
+      _this.log("HTTP power function failed");
+      callback(err);
+  });
+}
+
+httpSwitchWebhookAccessory.prototype.identify = function (callback) {
+  this.log("Identify requested!");
+  callback(); // success
+}
